5397d4c7b1dc861664dee510b5951b12c46096da
TheFilimisco <54910978l@iespoblenou.org>
2025-02-12 15:38:39 +0100

    add new package UI with new tasks

---
+++ src/ui/UI.java
@@ -0,0 +1,24 @@
+package ui;
+
+import model.user.User;
+
+import java.util.Scanner;
+
+public class UI {
+
+    public void addNewUser(Scanner input){
+        //Make user
+        //Decide what type User
+        //Call Register
+    }
+
+    public User loginInput(Scanner input){
+        //Input password and nickname
+        //return a User
+        return null;
+    }
+
+
+
+
+}
